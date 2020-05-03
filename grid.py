a80ab8ac3c5eb9456a7a7dc29597968028adedd3
joshhait <joshuahait@gmail.com>
2020-05-02 17:24:10 -0700

    added files
    whoops I should have used github this whole time but I forgot

---
+++ grid.py
@@ -0,0 +1,186 @@
+import pygame
+import math
+import util
+import time
+
+'''
+VALUES OF GRID:
+    0 - Empty square
+    1 - Wall
+    2 - Source
+    3 - Target
+    4 - Discovered
+    5 - Explored
+    6 - Path
+'''
+
+class Grid:
+    def __init__(self, WIN_SIZE, CELL_DIM):
+        self.rows = math.floor(WIN_SIZE[1]/(CELL_DIM[1]+CELL_DIM[2]))
+        self.cols = math.floor(WIN_SIZE[0]/(CELL_DIM[0]+CELL_DIM[2]))
+
+        self.grid = [[0 for x in range(self.cols)] for y in range(self.rows)]
+
+        self.source = (None, None)
+        self.target = (None, None)
+
+        self.cellWidth = CELL_DIM[0]
+        self.cellHeight = CELL_DIM[1]
+        self.cellMargin = CELL_DIM[2]
+
+    def getGrid(self):
+        return self.grid
+
+    def getElement(self, row, col):
+        return self.grid[row][col]
+
+    def setElement(self, row, col, val):
+        self.grid[row][col] = val
+
+    def setWall(self, row, col):
+        """
+            Sets a wall at grid[row][col]. Can't set a wall on top of the source/target square.
+        """
+        if self.grid[row][col] != 2 and self.grid[row][col] != 3:
+            self.grid[row][col] = 1
+            print("Wall set at (", row, ", ", col, ")")
+    
+    def removeWall(self, row, col):
+        self.grid[row][col] = 0
+
+    def getRows(self):
+        return self.rows
+
+    def getCols(self):
+        return self.cols
+
+    def reset(self):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                self.grid[row][col] = 0
+        self.source = (None, None)
+        self.target = (None, None)
+    
+    def getSource(self):
+        return self.source
+
+    def setSource(self, row, col):
+        self.source = (row, col)
+        self.grid[row][col] = 2
+
+    def removeSource(self):
+        row = self.source[0]
+        col = self.source[1]
+        self.grid[row][col] = 0
+        self.source = (None, None)
+
+    def getTarget(self):
+        return self.target
+
+    def setTarget(self, row, col):
+        self.target = (row, col)
+        self.grid[row][col] = 3
+
+    def removeTarget(self):
+        row = self.target[0]
+        col = self.target[1]
+        self.grid[row][col] = 0
+        self.target = (None, None)
+
+    def isTarget(self, node):
+        return (node == self.target)
+
+    def drawGrid(self, screen):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                color = (255, 255, 255)
+                if self.grid[row][col] == 1:
+                    color = (0, 0, 0)
+                elif self.grid[row][col] == 2:
+                    color = (0, 255, 0)
+                elif self.grid[row][col] == 3:
+                    color = (255, 0, 0)
+                elif self.grid[row][col] == 4:
+                    color = (0, 206, 209)
+                elif self.grid[row][col] == 5:
+                    color = (0, 0, 128)
+                elif self.grid[row][col] == 6:
+                    color = (255, 255, 0)
+                pygame.draw.rect(   screen, 
+                                    color, 
+                                    [(self.cellMargin + self.cellWidth) * col + self.cellMargin, 
+                                    (self.cellMargin + self.cellHeight) * row + self.cellMargin, 
+                                    self.cellWidth, 
+                                    self.cellHeight])
+
+    def getSuccessors(self, node):
+        row, col = node[0], node[1]
+        successors = []
+        if row - 1 >= 0 and self.grid[row-1][col] != 1:
+            pos = (row-1, col)
+            north = (pos, 'North', 1)
+            successors.append(north)
+        if row + 1 < self.rows and self.grid[row+1][col] != 1:
+            pos = (row+1, col)
+            south = (pos, 'South', 1)
+            successors.append(south)
+        if col + 1 < self.cols and self.grid[row][col+1] != 1:
+            pos = (row, col+1)
+            east = (pos, 'East', 1)
+            successors.append(east)
+        if col - 1 >= 0 and self.grid[row][col-1] != 1:
+            pos = (row, col-1)
+            west = (pos, 'West', 1)
+            successors.append(west)
+        return successors
+
+    def BFS(self):
+        """
+            Uses BFS to find path from source node to target node.
+            TODO: add visuals (i.e. change values  of grid so that draw shows it "visualizing")
+        """
+        if self.source == (None, None) or self.target == (None, None):
+            print("Source or Target is not set! Aborting BFS...")
+            return False
+
+        targetFound = False
+
+        explored, path = [], []
+
+        startPos = self.getSource()
+
+        fringe = util.Queue()
+
+        fringe.push((startPos, path))
+
+        while not fringe.isEmpty():
+            currNode, currPath = fringe.pop()
+
+            if currNode in explored:
+                continue
+
+            explored.append(currNode)
+            if currNode != self.getSource() and currNode != self.getTarget():
+                self.grid[currNode[0]][currNode[1]] = 4
+
+            if self.isTarget(currNode):
+                targetFound = True
+                break
+
+            for succ in self.getSuccessors(currNode):
+                nextXY = succ[0]
+                nextDir = succ[1]
+                nextCost = succ[2]
+                if nextXY != self.getSource() and nextXY != self.getTarget():
+                    self.grid[nextXY[0]][nextXY[1]] = 5
+
+                pathToSucc = currPath + [nextXY]
+
+                fringe.push((nextXY, pathToSucc))
+
+        if targetFound:
+            for node in currPath:
+                if node != self.getTarget():
+                    self.grid[node[0]][node[1]] = 6
+
+        return []
---
+++ main.py
@@ -0,0 +1,121 @@
+import pygame
+import math
+from grid import Grid
+
+
+'''
+Pathfinding Algorithms todo: BFS, DFS, Astar, Dijkstras
+
+Project TODO:
+    - probably need to expand the grid to be a list of lists of node objects, we'll see once we start implementing pathfinding
+
+'''
+
+def main():
+    # Width and height of the window
+    WIN_SIZE = (800, 600)
+
+    # Dimensions for Cells
+    # WIDTH, HEIGHT, MARGIN
+    CELL_DIM = (20, 20, 5)
+
+    grid = Grid(WIN_SIZE, CELL_DIM)
+
+    # initialize pygame
+    pygame.init()
+
+    # set up the screen and screen caption
+    screen = pygame.display.set_mode(WIN_SIZE)
+    pygame.display.set_caption("Pathfinding Visualizer")
+
+    # clock object to limit speed
+    clock = pygame.time.Clock()
+
+    isRunning = True
+
+    drag = False
+
+    while isRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                isRunning = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                drag = True
+                pos = pygame.mouse.get_pos()
+                col = pos[0] // (CELL_DIM[0] + CELL_DIM[2])
+                row = pos[1] // (CELL_DIM[1] + CELL_DIM[2])
+                grid.setWall(row, col)
+            elif event.type == pygame.MOUSEMOTION:
+                if drag:
+                    pos = pygame.mouse.get_pos()
+                    col = pos[0] // (CELL_DIM[0] + CELL_DIM[2])
+                    row = pos[1] // (CELL_DIM[1] + CELL_DIM[2])
+                    grid.setWall(row, col)
+            elif event.type == pygame.MOUSEBUTTONUP:
+                drag = False
+            elif event.type == pygame.KEYDOWN:
+                # Pressing the r key resets the entire grid
+                if event.key == pygame.K_r:
+                    grid.reset()
+                # Pressing the s key sets the source square
+                elif event.key == pygame.K_s:
+
+                    # get the row/col of the square where the mouse is
+                    pos = pygame.mouse.get_pos()
+                    col = pos[0] // (CELL_DIM[0] + CELL_DIM[2])
+                    row = pos[1] // (CELL_DIM[1] + CELL_DIM[2])
+
+                    # if the source is not set, set it
+                    if grid.getSource() == (None, None):
+                        grid.setSource(row, col)
+                    # if the source is set
+                    else:
+                        # if the source is the current square, remove it
+                        if grid.getElement(row, col) == 2:
+                            grid.removeSource()
+                        # if the source is not the current square, remove it, and make current square the source
+                        elif grid.getElement(row, col) == 0 or grid.getElement(row, col) == 1:
+                            grid.removeSource()
+                            grid.setSource(row, col)
+                    node = (row, col)
+                    print(grid.getSuccessors(node))
+                    print("rows: ", grid.getRows(), " | cols: ", grid.getCols())
+                # Pressing the t key sets the target square
+                elif event.key == pygame.K_t:
+
+                    # get the row/col of the square where the mouse is
+                    pos = pygame.mouse.get_pos()
+                    col = pos[0] // (CELL_DIM[0] + CELL_DIM[2])
+                    row = pos[1] // (CELL_DIM[1] + CELL_DIM[2])
+
+                    # if the target is not set, set it
+                    if grid.getTarget() == (None, None):
+                        grid.setTarget(row, col)
+                    # if the target is set
+                    else:
+                        # if the target is the current square, remove it
+                        if grid.getElement(row, col) == 3:
+                            grid.removeTarget()
+                        # if the target is not the current square, remove it, and make current square the target
+                        elif grid.getElement(row, col) == 0  or grid.getElement(row, col) == 1: 
+                            grid.removeTarget()
+                            grid.setTarget(row, col)
+                elif event.key == pygame.K_1:
+                    grid.BFS()
+
+        # tock here and update tick to tock
+
+        draw(grid, screen)
+
+        clock.tick(60)
+
+    pygame.quit()
+
+def draw(grid, screen):
+    screen.fill((105, 105, 105))
+    grid.drawGrid(screen)
+    pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
